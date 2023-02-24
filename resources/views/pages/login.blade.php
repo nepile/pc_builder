fd9fce81e5696b4c35befb41b7acae035ec4b621
nepile <nevillejro.laia@gmail.com>
2023-02-24 22:43:36 +0700

    add bootstrap5.2 asset

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="{{ asset('css/bs5.css') }}">
 </head>
 <body>
     
+    @yield('content')
+
+    <script src="{{ asset('js/bs5.js') }}"></script>
 </body>
 </html>
